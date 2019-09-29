0d95220f22ed81c17cbf81f8a0a3b2c12d18ce31
Jae-eun <je4297@naver.com>
2019-09-29 18:11:39 +0900

    Add Monthly

---
+++ XX/Monthly/MonthlyDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MonthlyDetailViewController.swift
+//  XX
+//
+//  Created by 이재은 on 29/09/2019.
+//  Copyright © 2019 ebpark. All rights reserved.
+//
+
+import UIKit
+
+class MonthlyDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ XX/Monthly/MonthlyViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MonthlyViewController.swift
+//  XX
+//
+//  Created by 이재은 on 29/09/2019.
+//  Copyright © 2019 ebpark. All rights reserved.
+//
+
+import UIKit
+
+class MonthlyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
