3ae221d3de0c809c3c8054425672ce0c70ea89b4
박은비 <ebpark9511@gmail.com>
2019-10-27 23:26:55 +0900

    Main Header rx 적용

--- XX/Main/ViewController/MainHeaderReusableView.swift
+++ XX/Main/ViewController/MainHeaderReusableView.swift
@@ -13,97 +13,98 @@ import CenteredCollectionView
 class MainHeaderReusableView: UICollectionReusableView {
     
     let cellPercentWidth: CGFloat = 0.83
+    let cellPercntSpacing: CGFloat = 0.037
     
-    let cellPercentWidth2: CGFloat = 0.037
     var centeredCollectionViewFlowLayout: CenteredCollectionViewFlowLayout!
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    // 뷰모델 너무 많아질것을 고려하여 MainViewModel 갖다씀
+    var viewModel: MainViewModel?
+    let bag = DisposeBag()
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        setLayoutCollectionView()
+        bindCollectionView()
+    }
     
+    func setLayoutCollectionView() {
         centeredCollectionViewFlowLayout = (collectionView.collectionViewLayout as! CenteredCollectionViewFlowLayout)
         collectionView.decelerationRate = UIScrollView.DecelerationRate.fast
-        
-        self.applyGradient()
-        
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
         
         centeredCollectionViewFlowLayout.itemSize = CGSize(
             width: UIScreen.main.bounds.width * cellPercentWidth,
             height: 158 // fix value
         )
         
-        centeredCollectionViewFlowLayout.minimumLineSpacing = UIScreen.main.bounds.width * cellPercentWidth2
-        
-
+        centeredCollectionViewFlowLayout.minimumLineSpacing = UIScreen.main.bounds.width * cellPercntSpacing
     }
 }
 
-extension MainHeaderReusableView: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("Selected Cell #\(indexPath.row)")
-    }
-}
 
-extension MainHeaderReusableView: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+extension MainHeaderReusableView: ViewModelBindableType {
+    func bindViewModel() {
+//        guard let viewModel = viewModel else { return }
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: "CollectionViewCell"), for: indexPath) as! MainViewCollectionViewCell
-        cell.label.text = "Cell #\(indexPath.row)"
+    private func bindCollectionView() {
+        let dummyData = ["page - 1", "page - 2"]
         
-        cell.contentView.layer.cornerRadius = 15.0
-        cell.contentView.layer.borderWidth = 1.0
-        cell.contentView.layer.borderColor = UIColor.clear.cgColor
-        cell.contentView.layer.masksToBounds = true
-        cell.layer.shadowColor = UIColor.lightGray.cgColor
-        cell.layer.shadowOffset = CGSize(width: 1, height: 4.0)
-        cell.layer.shadowRadius = 2.0
-        cell.layer.shadowOpacity = 10.0
-        
-        cell.layer.masksToBounds = false
-        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds,
-                                             cornerRadius: cell.contentView.layer.cornerRadius).cgPath
-        
-        return cell
-    }
-    
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        print("Current centered index: \(String(describing: centeredCollectionViewFlowLayout.currentCenteredPage ?? nil))")
-    }
-    
-    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
-        print("Current centered index: \(String(describing: centeredCollectionViewFlowLayout.currentCenteredPage ?? nil))")
-    }
-}
-
-extension UIView {
-    func applyGradient() {
-        let gradient = CAGradientLayer()
-        gradient.frame = bounds
-        gradient.locations = [0, 1]
-        gradient.startPoint = CGPoint(x: 0, y: 0)
-        gradient.endPoint = CGPoint(x: 1, y: 1)
-        gradient.colors = [
-            UIColor.peach.cgColor,
-            UIColor.orange.cgColor
+        let sections = [
+            SectionModel<String, String>(model: "first section", items: dummyData)
         ]
-        layer.insertSublayer(gradient, at: 0)
-    }
-}
-
-
-extension UIColor {
-    static var peach: UIColor {
-        return  UIColor(red: 1, green: 0.5764705882, blue: 0.5843137255, alpha: 1)
+         
+        collectionView
+            .rx.didEndDecelerating
+            .bind { () in
+//                print("Current centered index: \(String(describing: self.centeredCollectionViewFlowLayout.currentCenteredPage ?? nil))")
+            }.disposed(by: bag)
+        
+        collectionView
+            .rx.didEndScrollingAnimation
+            .bind { () in
+//              print("Current centered index: \(String(describing: self.centeredCollectionViewFlowLayout.currentCenteredPage ?? nil))")
+        }.disposed(by: bag)
+        
+        collectionView
+            .rx.itemSelected.bind { (indexPath) in
+//                print("indexPath >>> \(indexPath.item)")
+        }.disposed(by: bag)
+        
+        Observable.just(sections)
+            .bind(to: collectionView.rx.items(dataSource: mainDatasource))
+            .disposed(by: bag)
     }
     
-    static var orange: UIColor {
-        return UIColor(red: 1, green: 0.5764705882, blue: 0.462745098, alpha: 1)
+    typealias MainHeaderReusableSectionModel = SectionModel<String, String>
+    typealias MainHeaderCollectionViewDataSource = RxCollectionViewSectionedReloadDataSource<MainHeaderReusableSectionModel>
+    
+    private var mainDatasource: MainHeaderCollectionViewDataSource {
+        let configureCell: (CollectionViewSectionedDataSource<MainHeaderReusableSectionModel>, UICollectionView, IndexPath, String) -> UICollectionViewCell =
+        { (datasource, collectionView, indexPath,  element) in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: "CollectionViewCell"), for: indexPath) as! MainViewCollectionViewCell
+            cell.label.text = "Cell #\(element)"
+            
+            cell.contentView.layer.cornerRadius = 15.0
+            cell.contentView.layer.borderWidth = 1.0
+            cell.contentView.layer.borderColor = UIColor.clear.cgColor
+            cell.contentView.layer.masksToBounds = true
+            cell.layer.shadowColor = UIColor.lightGray.cgColor.copy(alpha: 0.3)
+            cell.layer.shadowOffset = CGSize(width: -3.0, height: 2.0)
+            cell.layer.shadowRadius = 4.0
+            cell.layer.shadowOpacity = 10
+            
+            cell.layer.masksToBounds = false
+            cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds,
+                                                 cornerRadius: cell.contentView.layer.cornerRadius).cgPath
+            
+            return cell
+        }
+        
+        let datasource = MainHeaderCollectionViewDataSource.init(configureCell: configureCell)
+        
+        return datasource
     }
 }
+
--- XX/Main/ViewController/MainViewController.swift
+++ XX/Main/ViewController/MainViewController.swift
@@ -16,7 +16,6 @@ class MainViewController: BaseViewController {
     
     let HEADER_CELL_NAME = "MainHeaderCell"
     let SUB_HEADER_CELL_NAME = "MainSubHeaderCell"
-    
     let CELL_NAME = "MainCell"
     
     var viewModel: MainViewModel?
@@ -26,6 +25,7 @@ class MainViewController: BaseViewController {
         super.viewDidLoad()
         bindCollectionView()
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
@@ -38,18 +38,16 @@ extension MainViewController: ViewModelBindableType {
         addSpendMoveButton.rx.action = viewModel.requestAddSpendMoveMoveAction()
     }
     
-
-    
     private func bindCollectionView() {
-        
-        let firstCities = ["default", "London", "Vienna", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon"]
-//        let secondCities = ["Paris", "Madrid", "Seoul"]
+        let dummyData = ["default", "London", "Vienna", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon", "Lisbon"]
         
         let sections = [
-            SectionModel<String, String>(model: "first section", items: firstCities)
+            SectionModel<String, String>(model: "first section", items: dummyData)
         ]
         
-        
+        collectionView
+        .rx.setDelegate(self)
+        .disposed(by: bag)
         
         Observable.just(sections)
             .bind(to: collectionView.rx.items(dataSource: mainDatasource))
@@ -57,38 +55,30 @@ extension MainViewController: ViewModelBindableType {
     }
     
     typealias MainSectionModel = SectionModel<String, String>
-    typealias MainDataSource = RxCollectionViewSectionedReloadDataSource<MainSectionModel>
+    typealias MainCollectionViewDataSource = RxCollectionViewSectionedReloadDataSource<MainSectionModel>
     
-    private var mainDatasource: MainDataSource {
-        let configureCell: (CollectionViewSectionedDataSource<MainSectionModel>, UICollectionView, IndexPath, String) -> UICollectionViewCell = { (datasource, collectionView, indexPath,  element) in
-            //            guard
-            
+    private var mainDatasource: MainCollectionViewDataSource {
+        let configureCell: (CollectionViewSectionedDataSource<MainSectionModel>, UICollectionView, IndexPath, String) -> UICollectionViewCell =
+        { (datasource, collectionView, indexPath,  element) in
             if indexPath.item == 0 {
                 let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.SUB_HEADER_CELL_NAME, for: indexPath)
                 return cell
             } else {
                 let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.CELL_NAME, for: indexPath)
-                //                    as? NameCell else { return UITableViewCell() }
-                
-                //            cell.textLabel?.text = element
-                
                 return cell
             }
         }
         
-        let datasource = MainDataSource.init(configureCell: configureCell)
+        let datasource = MainCollectionViewDataSource.init(configureCell: configureCell)
         
-        datasource.configureSupplementaryView = {(dataSource, collectionView, kind, indexPath) -> UICollectionReusableView in
+        datasource.configureSupplementaryView = {
+            (dataSource,
+            collectionView,
+            kind,
+            indexPath) -> UICollectionReusableView in
             let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: self.HEADER_CELL_NAME, for: indexPath)
-            //                as! HeaderCell
-            //            header.setup()
             return header
         }
-        
-//        sizeForItemAtIndexPath
-        
-    
-
         
         return datasource
         
@@ -96,38 +86,16 @@ extension MainViewController: ViewModelBindableType {
     
 }
 
-//extension MainViewController: UICollectionViewDelegate {
-//    // 섹션 수 설정
-//    func numberOfSections(in collectionView: UICollectionView) -> Int {
-//        return 1
-//    }
-//    
-//    // 섹션에 따라 셀 수 정함
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 2
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CELL_NAME, for: indexPath as IndexPath)
-//        return cell
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        print("Cell \(indexPath.row) selected")
-//    }
-//}
-//
-//
-//extension MainViewController: UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-//        if kind == UICollectionView.elementKindSectionHeader {
-//            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader,
-//                                                                             withReuseIdentifier:HEADER_CELL_NAME,
-//                                                                             for: indexPath)
-//            
-//            return headerView
-//        } else {
-//            return UICollectionReusableView()
-//        }
-//    }
-//}
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if indexPath.item == 0 {
+            return CGSize(width: UIScreen.main.bounds.width,
+                               height: 116)
+        }
+        
+        return CGSize(width: UIScreen.main.bounds.width * 0.408,
+                      height: UIScreen.main.bounds.width * 0.408)
+    }
+}
+
