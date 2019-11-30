9281d8248c026d572962b03df8cd22335c6b72de
Seonghun <kim.seonghun23@gmail.com>
2019-11-30 14:29:13 +0900

    버그 수정

--- XX/initialize/ViewController/CategoriesSettingViewController.swift
+++ XX/initialize/ViewController/CategoriesSettingViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CategoriesSettingViewController: BaseViewController {
+class CategoriesSettingViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var budgetLabel: UILabel!
